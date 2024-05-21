e6c2836d1352e69c7ebaa2b6a06c51df279cbf11
JuanL <laravega.juan8@gmail.com>
2024-05-21 00:46:03 -0400

    update

--- src/test/java/gradedStudents/StudentTest.java
+++ src/test/java/gradedStudents/StudentTest.java
@@ -31,13 +31,27 @@ class StudentTest {
 
     @Test
     public void testSetExamScore() {
-
+        ArrayList<Double> examScores;
+        examScores = new ArrayList<>();
+        Student student = new Student("Kamado","Tanjiro",examScores);
+        student.addExamScore(90.0);
+        student.addExamScore(85.5);
+        student.setExamScore(2, 88.0);
+        String expected = "Exam scores:\n\tExam 1 -> 90.0\n\tExam 2 -> 88.0\n";
+        assertEquals(expected, student.getExamScore());
     }
 
 
     @Test
     public void testGetAverageExamScore() {
-
+        ArrayList<Double> examScores;
+        examScores = new ArrayList<>();
+        Student student = new Student("Vinsmoke","Sanji",examScores);
+        student.addExamScore(93.0);
+        student.addExamScore(88.0);
+        student.addExamScore(85.0);
+        double expected = 88.67;
+        assertEquals(expected, student.getAverageExamScores(), 0.01);
     }
 
 }
